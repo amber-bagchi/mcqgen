3d7d37db1c75d4c56403e77aeced8000af1f4ee8
Amber Bagchi <amberbagchi2604@gmail.com>
2024-07-27 16:24:16 +0530

    i created alogger

---
+++ src/mcqgenerator/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
+
+log_path=os.path.join(os.getcwd(),"logs") #path of where to store the log files
+
+os.makedirs(log_path,exist_ok=True) #creating the log folder
+
+LOG_FILEPATH=os.path.join(log_path,LOG_FILE)
+
+
+logging.basicConfig(level=logging.INFO, 
+        filename=LOG_FILEPATH,
+        format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
+)
+
